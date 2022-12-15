665b702ec021bcd3bd9641c47fd964686f231cb6
Alebecito <alebe12345@gmail.com>
2022-12-15 17:31:30 -0600

    Labels de login cambiadas

--- frontend/src/layouts/login/index.js
+++ frontend/src/layouts/login/index.js
@@ -43,10 +43,10 @@ function Basic() {
         <MDBox pt={4} pb={3} px={3}>
           <MDBox component="form" role="form">
             <MDBox mb={2}>
-              <MDInput type="email" label="Email" fullWidth />
+              <MDInput type="email" label="Correo electrónico" fullWidth />
             </MDBox>
             <MDBox mb={2}>
-              <MDInput type="password" label="Password" fullWidth />
+              <MDInput type="password" label="Contraseña" fullWidth />
             </MDBox>
             <MDBox mt={4} mb={1}>
               <MDButton
